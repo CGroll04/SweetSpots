46c21a4ee94777e5e91df0cf7f1966def5bb076f
CGroll04 <charliegroll04@gmail.com>
2025-02-12 22:38:32 -0500

    Started Sign in and Sign up Pages

--- SweetSpots/Core/Authentication/SignInView.swift
+++ SweetSpots/Core/Authentication/SignInView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                //logo
+                Image(systemName: "map")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .padding(.vertical, 50)
+                
+                //form fields
+                
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top,75)
+                
+                //sign in button
+                
+                Button{
+                    print("Log User In...")
+                } label: {
+                    HStack{
+                        Text("Sign In")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: 150, height: 48)
+                }
+                .background(Color(.systemBlue))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.top, 24)
+                
+                Spacer()
+                //sign up button
+                
+                NavigationLink{
+                    SignUpView()
+                        .navigationBarBackButtonHidden()
+                }label: {
+                    HStack(spacing: 3){
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size:14))
+                }
+            }
+        }
     }
 }
 
--- SweetSpots/Core/Authentication/SignUpView.swift
+++ SweetSpots/Core/Authentication/SignUpView.swift
@@ -8,8 +8,73 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var email = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //logo
+            Image(systemName: "map")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .padding(.vertical, 20)
+            //form fields
+            
+            VStack(spacing: 24) {
+                InputView(text: $email,
+                          title: "Email Address",
+                          placeholder: "name@example.com")
+                
+                InputView(text: $fullname,
+                          title: "Full Name",
+                          placeholder: "Enter your name")
+            
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your password",
+                          isSecureField: true)
+                
+                InputView(text: $confirmPassword,
+                          title: "Confirm Password",
+                          placeholder: "Confirm your password",
+                          isSecureField: true)
+            }
+            .padding(.horizontal)
+            .padding(.top,12)
+            
+            Button{
+                print("Sign User Up...")
+            } label: {
+                HStack{
+                    Text("Sign Up")
+                        .fontWeight(.semibold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundStyle(.white)
+                .frame(width: 150, height: 48)
+            }
+            .background(Color(.systemBlue))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.top, 24)
+            
+            Spacer()
+            //sign up button
+            
+            Button{
+                dismiss()
+            }label: {
+                HStack(spacing: 3){
+                    Text("Already have an account?")
+                    Text("Sign In")
+                        .fontWeight(.bold)
+                }
+                .font(.system(size:14))
+            }
+        }
     }
 }
 
