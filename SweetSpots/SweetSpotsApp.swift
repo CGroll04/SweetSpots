d3f174c11365370257c3f1a8cd1cf4cfc0a7535c
CGroll04 <charliegroll04@gmail.com>
2025-05-04 17:14:06 -0400

    Initial Commit

---
+++ SweetSpots/SweetSpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSpotsApp.swift
+//  SweetSpots
+//
+//  Created by Charlie Groll on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct SweetSpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SweetSpotsTests/SweetSpotsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSpotsTests.swift
+//  SweetSpotsTests
+//
+//  Created by Charlie Groll on 2025-05-04.
+//
+
+import Testing
+@testable import SweetSpots
+
+struct SweetSpotsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
